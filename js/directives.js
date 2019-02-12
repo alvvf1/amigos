d3145e5579b7f059e8824005284c9648c35743b9
Al Tfe <heroku7@gmx.es>
2019-02-12 06:41:59 +0000

    primer commit

---
+++ js/directives.js
@@ -0,0 +1,42 @@
+var app = angular.module('benchmates');
+
+app.directive('buttonSendMessage', function () {
+    return {
+        restrict: 'E',
+        scope: {
+            profile: '='
+        },
+        template: '<a href="#/messages/{{profile.id}}" role="button" class="btn btn-secondary btn-sm"><span class="glyphicon glyphicon-envelope"></span> Enviar mensaje</a>',
+        replace: true
+    };
+});
+
+app.directive('buttonRemoveFriend', function () {
+    return {
+        restrict: 'E',
+        scope: {
+            account: '=',
+            profile: '=',
+            remove: '&'
+        },
+        template: '<a role="button" class="btn btn-secondary btn-sm" ng-show="account.hasFriend(profile.id)" ' +
+        'ng-class="profile === account ? \'link-disabled\' : \'\'" ng-click="remove()">' +
+        '<span class="glyphicon glyphicon-minus-sign" title=""></span> Eliminar amigo</a>',
+        replace: true
+    };
+});
+
+app.directive('buttonAddFriend', function () {
+    return {
+        restrict: 'E',
+        scope: {
+            account: '=',
+            profile: '=',
+            add: '&'
+        },
+        template: '<a role="button" class="btn btn-secondary btn-sm" ng-show="!account.hasFriend(profile.id)" ' +
+        'ng-class="profile === account ? \'link-disabled\' : \'\'" ng-click="add()">' +
+        '<span class="glyphicon glyphicon-minus-sign" title=""></span> Añadir amigo</a>',
+        replace: true
+    };
+});
